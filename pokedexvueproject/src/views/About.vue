fac0a6c50f1912e678f87e12543b715790c2303b
angcastillo18 <angcastillo18@gmail.com>
2020-01-17 23:06:33 -0500

    added the page About, and added styles to the page Home

--- pokedexvueproject/src/views/About.vue
+++ pokedexvueproject/src/views/About.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
-        <p>Hola pendejo</p>
+        <b-img  :src="require('../assets/Banner-pokemon.jpg')" class="img-fluid mt-5" style="max-height: 300px;  " alt="img failed"/>
+        <b-card bg-variant="danger" text-variant="white" header="Mi pokedex">
+            <b-card-text>Creado con Vue.js, Bootstrap, Vuex, Router y Pokemon Api.</b-card-text>
+            <p>Puedes descargar el proyecto en el siguiente link: 
+                <b-link href="https://github.com/angcastillo18/PokedexVue-project">Repositorio del proyecto</b-link>
+            </p>
+        </b-card>   
     </div>
 </template>
 
@@ -10,6 +16,5 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
 </style>
--- pokedexvueproject/src/views/Home.vue
+++ pokedexvueproject/src/views/Home.vue
@@ -1,18 +1,73 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container class="mt-4">
+      <h1 class="text-center">Mis pokemones</h1>
+      <b-form class="row  search-container">
+        <b-input class="col-9" v-model="text" placeholder="Ingresa el nombre del pokemon:">
+        </b-input>
+        <b-button class="col-2 mx-2" variant="primary" >Buscar</b-button>
+      </b-form>
+    <div class="mt-2">Value: {{ text }}</div>
+    <b-list-group>
+      <b-list-group-item class="pokemon-content">
+        <b-row>
+          <b-col class="col-12">
+            <b-img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" ></b-img>
+          </b-col>
+          <b-col class="col-12">
+            <h1>Pikachu</h1>
+          </b-col>
+        </b-row>        
+      </b-list-group-item>
+            <b-list-group-item class="pokemon-content">
+        <b-row>
+          <b-col class="col-12">
+            <b-img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" ></b-img>
+          </b-col>
+          <b-col class="col-12">
+            <h1>Pikachu</h1>
+          </b-col>
+        </b-row>        
+      </b-list-group-item>
+            <b-list-group-item class="pokemon-content">
+        <b-row>
+          <b-col class="col-12">
+            <b-img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" ></b-img>
+          </b-col>
+          <b-col class="col-12">
+            <h1>Pikachu</h1>
+          </b-col>
+        </b-row>        
+      </b-list-group-item>
+    </b-list-group>
+
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    //HelloWorld
+  },
+  data() {
+      return {
+        text: ''
+      }
   }
 }
 </script>
+
+<style  scoped>
+.search-container{
+ max-width: 750px;
+ margin: 25px auto;
+}
+.pokemon-content{
+  max-width: 200px
+}
+</style>
